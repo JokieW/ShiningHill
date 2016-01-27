cdb4fda31fc353205156dd6a2ca085a96d685d66
JokieW <gulliverjko@gmail.com>
2016-01-27 06:32:42 -0500

    First commit

---
+++ Assets/Editor/Detective.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+public class Detective : EditorWindow
+{
+    static EditorWindow _currentWindow;
+
+    [MenuItem("SilentParty/Detective")]
+    public static void ShowWindow()
+    {
+        _currentWindow = EditorWindow.GetWindow(typeof(Detective));
+    }
+
+    bool _initialised = false;
+    static Archive _currentArchive;
+    Object _currentFile;
+
+    void Init()
+    {
+        _currentArchive = new Archive(null);
+        _initialised = true;
+    }
+
+    void OnGUI()
+    {
+        if (!_initialised || _currentArchive == null)
+        {
+            Init();
+        }
+
+        _currentFile = EditorGUILayout.ObjectField("Archive", _currentFile, typeof(Object), false);
+
+        if (GUILayout.Button("Open"))
+        {
+            _currentArchive.File = _currentFile;
+            _currentArchive.OpenArchive();
+        }
+        if (_currentArchive.AllFiles != null)
+        {
+            foreach (KeyValuePair<string, Archive.ArcFile> kvp in _currentArchive.AllFiles)
+            {
+                GUILayout.Label("__________________________");
+                GUILayout.Label(kvp.Key);
+                GUILayout.Label("Offset " + kvp.Value.Offset);
+                GUILayout.Label("FileID " + kvp.Value.FileID);
+                GUILayout.Label("Length " + kvp.Value.Length);
+                GUILayout.Label("Length2 " + kvp.Value.Lenght2);
+            }
+        }
+
+    }
+}
---
+++ Assets/src/Archive.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+public class Archive
+{
+    private Object _archiveFile;
+
+    public Object File
+    {
+        get
+        {
+            return _archiveFile;
+        }
+        set
+        {
+            _archiveFile = value;
+        }
+    }
+
+    ArcHeader _header = null;
+    Dictionary<string, ArcFile> _files = null;
+    public Dictionary<string, ArcFile> AllFiles
+    {
+        get
+        {
+            return _files;
+        }
+    }
+
+    public Archive(Object file)
+    {
+        _archiveFile = file;
+    }
+
+    public bool IsLoaded()
+    {
+        return _header != null && _files != null;
+    }
+
+    public void OpenArchive()
+    {
+        string assetPath = AssetDatabase.GetAssetPath(_archiveFile);
+        BinaryReader reader = null;
+        try
+        {
+            reader = new BinaryReader(new FileStream(assetPath, FileMode.Open, FileAccess.Read, FileShare.Read));
+            _header = new ArcHeader(reader);
+
+            _files = new Dictionary<string, ArcFile>((int)_header.fileCount);
+            for (int fileI = 0; fileI != (int)_header.fileCount; fileI++)
+            {
+                ArcFile file = new ArcFile(reader);
+                string name = String.Format("File {0} ({1})", fileI, file.FileID);
+                _files.Add(name, file);
+            }
+        }
+        catch(Exception e)
+        {
+            Debug.LogError("Path " + assetPath);
+            Debug.LogException(e);
+            if (reader != null)
+            {
+                reader.Close();
+            }
+        }
+    }
+
+    public void UnloadArchive()
+    {
+        _header = null;
+        _files = null;
+    }
+
+    public class ArcHeader
+    {
+        public UInt32 firstUint32;
+        public UInt32 fileCount;
+        public UInt32 paddingLength;
+        public UInt32 fourthUint32;
+
+        public ArcHeader(BinaryReader reader)
+        {
+            reader.BaseStream.Position = 0L;
+            firstUint32 = reader.ReadUInt32();
+            fileCount = reader.ReadUInt32();
+            paddingLength = reader.ReadUInt32();
+            fourthUint32 = reader.ReadUInt32();
+        }
+    }
+
+    public class ArcFile
+    {
+        public UInt32 Offset;
+        public UInt32 FileID; //Unknown
+        public UInt32 Length;
+        public UInt32 Lenght2; //Not quite sure why there is two length
+        public byte[] data;
+
+        public ArcFile(BinaryReader reader)
+        {
+            Offset = reader.ReadUInt32();
+            FileID = reader.ReadUInt32();
+            Length = reader.ReadUInt32();
+            Lenght2 = reader.ReadUInt32();
+
+            long currentSeek = reader.BaseStream.Position;
+            reader.BaseStream.Position = (long)Offset;
+            data = reader.ReadBytes((int)Length);
+            reader.BaseStream.Position = currentSeek;
+        }
+    }
+}
