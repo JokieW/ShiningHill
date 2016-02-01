d22eb1fe98973d210f1dc188adc41aa5c04776d0
JokieW <gulliverjko@gmail.com>
2016-01-31 22:18:47 -0500

    Made the hexDisplay options self contained

--- Assets/Editor/Detective.cs
+++ Assets/Editor/Detective.cs
@@ -7,103 +7,99 @@ using UnityEngine;
 using UnityEditor;
 using Object = UnityEngine.Object;
 
-public class Detective : EditorWindow
+namespace SilentParty
 {
-    static EditorWindow _currentWindow;
-
-    [MenuItem("SilentParty/Detective")]
-    public static void ShowWindow()
+    public class Detective : EditorWindow
     {
-        _currentWindow = EditorWindow.GetWindow(typeof(Detective));
-    }
+        static EditorWindow _currentWindow;
 
-    bool _initialised = false;
-    static Archive _currentArchive;
-    Object _currentFile;
-
-    int _selectedFile;
-    string[] _fileNames;
-    Vector2 _scrollPosition;
-
-    //Hex display
-    Vector2 _hexScroll;
-    HexDisplay.DisplayType _hexDisplayType = HexDisplay.DisplayType.ANSI;
-    int _hexBytesPerRow = 16;
-    int _hexGrouping = 4;
-
-    void Init()
-    {
-        _currentArchive = new Archive(null);
-        _initialised = true;
-    }
-
-    void OnGUI()
-    {
-        if (!_initialised || _currentArchive == null)
+        [MenuItem("SilentParty/Detective")]
+        public static void ShowWindow()
         {
-            Init();
+            _currentWindow = EditorWindow.GetWindow(typeof(Detective));
         }
 
-        _currentFile = EditorGUILayout.ObjectField("Archive", _currentFile, typeof(Object), false);
+        bool _initialised = false;
+        static Archive _currentArchive;
+        Object _currentFile;
 
-        if (GUILayout.Button("Test"))
+        int _selectedFile;
+        string[] _fileNames;
+        Vector2 _scrollPosition;
+
+        //Hex display
+        Vector2 _hexScroll;
+
+        void Init()
         {
-            for (int i = 0; i != 256; i++)
+            _currentArchive = new Archive(null);
+            _initialised = true;
+        }
+
+        void OnGUI()
+        {
+            if (!_initialised || _currentArchive == null)
             {
-                Debug.Log(i.ToString() + " [" + System.Convert.ToChar(i) + "]");
-            }
-        }
-
-        if (GUILayout.Button("Open"))
-        {
-            _currentArchive.File = _currentFile;
-            _currentArchive.OpenArchive();
-            _selectedFile = 0;
-            _fileNames = _currentArchive.AllFiles.Select(x => x.Key).ToArray();
-            _scrollPosition = Vector2.zero;
-        }
-        if (_currentArchive.AllFiles != null)
-        {
-            GUILayout.BeginHorizontal();
-
-            //File selection
-            GUILayout.BeginVertical(GUILayout.Width(150.0f));
-            _scrollPosition = GUILayout.BeginScrollView(_scrollPosition, false, true);
-
-            EditorGUI.BeginChangeCheck();
-            _selectedFile = GUILayout.SelectionGrid(_selectedFile, _fileNames, 1);
-            if(EditorGUI.EndChangeCheck())
-            {
-                _hexScroll = Vector2.zero;
+                Init();
             }
 
-            GUILayout.EndScrollView();
-            GUILayout.EndVertical();
+            _currentFile = EditorGUILayout.ObjectField("Archive", _currentFile, typeof(Object), false);
 
-            // File info
-            GUILayout.BeginVertical();
+            if (GUILayout.Button("Test"))
+            {
+                for (int i = 0; i != 256; i++)
+                {
+                    Debug.Log(i.ToString() + " [" + System.Convert.ToChar(i) + "]");
+                }
+            }
 
-            Archive.ArcFile file = _currentArchive.AllFiles[_fileNames[_selectedFile]];
-            GUILayout.Label("Offset " + file.Offset);
-            GUILayout.Label("FileID " + file.FileID);
-            GUILayout.Label("Length " + file.Length + " (" + file.Lenght2+")");
-            GUILayout.Label("Type UNKNOWN");
+            if (GUILayout.Button("Open"))
+            {
+                _currentArchive.File = _currentFile;
+                _currentArchive.OpenArchive();
+                _selectedFile = 0;
+                _fileNames = _currentArchive.AllFiles.Select(x => x.Key).ToArray();
+                _scrollPosition = Vector2.zero;
+            }
+            if (_currentArchive.AllFiles != null)
+            {
+                GUILayout.BeginHorizontal();
 
-            GUILayout.BeginHorizontal();
-            _hexBytesPerRow = EditorGUILayout.IntPopup("Bytes Per Row", _hexBytesPerRow, new string[] { "8", "16", "24", "32" }, new int[] { 8, 16, 24, 32 });
-            GUILayout.FlexibleSpace();
-            _hexGrouping = EditorGUILayout.IntPopup("Groupings", _hexGrouping, new string[] { "1", "2", "4", "8" }, new int[] { 1, 2, 4, 8 });
-            GUILayout.FlexibleSpace();
-            _hexDisplayType = (HexDisplay.DisplayType)EditorGUILayout.EnumPopup("Preview format", _hexDisplayType);
-            GUILayout.EndHorizontal();
+                //File selection
+                GUILayout.BeginVertical(GUILayout.Width(150.0f));
+                _scrollPosition = GUILayout.BeginScrollView(_scrollPosition, false, true);
 
-            _hexScroll = HexDisplay.Display(_hexScroll, file.data, _hexBytesPerRow, _hexGrouping, _hexDisplayType, null);
+                EditorGUI.BeginChangeCheck();
+                _selectedFile = GUILayout.SelectionGrid(_selectedFile, _fileNames, 1);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    _hexScroll = Vector2.zero;
+                }
 
-            GUILayout.EndVertical();
+                GUILayout.EndScrollView();
+                GUILayout.EndVertical();
 
-            GUILayout.EndHorizontal();
-        
+                // File info
+                GUILayout.BeginVertical();
+
+                Archive.ArcFile file = _currentArchive.AllFiles[_fileNames[_selectedFile]];
+                GUILayout.Label("Offset " + file.Offset);
+                GUILayout.Label("FileID " + file.FileID);
+                GUILayout.Label("Length " + file.Length + " (" + file.Lenght2 + ")");
+                GUILayout.Label("Type UNKNOWN");
+                if (GUILayout.Button("Try to recover as Scene"))
+                {
+                    Scene.AttemptRecovery(file);
+                }
+
+                _hexScroll = HexDisplay.Display(_hexScroll, file.data, null);
+
+                GUILayout.EndVertical();
+
+                GUILayout.EndHorizontal();
+
+            }
+
         }
-
     }
 }
--- Assets/Editor/HexDisplay.cs
+++ Assets/Editor/HexDisplay.cs
@@ -22,17 +22,17 @@ public class HexDisplay
 
     private static int _MAXROWS = 40;
 
-    public static Vector2 Display(Vector2 scroll, byte[] data, int bytesPerRow, int grouping, DisplayType dispType, GUILayoutOption[] layoutOptions)
+    private static string _goToAddressText = "";
+
+    static HexDisplay.DisplayType _dispType = HexDisplay.DisplayType.ANSI;
+    static int _bytesPerRow = 16;
+    static int _grouping = 4;
+
+    public static Vector2 Display(Vector2 scroll, byte[] data, GUILayoutOption[] layoutOptions)
     {
         try
         {
-            float _dataWidth = ((bytesPerRow * 2) + (bytesPerRow / grouping) - 1) * 10.25f;
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Offset (h)", GUILayout.Width(90.0f));
-            GUILayout.Label("Data", GUILayout.Width(_dataWidth));
-            GUILayout.Label("Preview");
-            GUILayout.EndHorizontal();
+            float _dataWidth = ((_bytesPerRow * 2) + (_bytesPerRow / _grouping) - 1) * 10.25f;
 
             StringBuilder indexSB = new StringBuilder();
             StringBuilder centerSB = new StringBuilder();
@@ -43,13 +43,13 @@ public class HexDisplay
             while (pointer < data.LongLength && currentRow <= _MAXROWS)
             {
                 indexSB.AppendFormat("{0:X8}", pointer);
-                long chunkSize = Min(bytesPerRow, data.LongLength - pointer);
+                long chunkSize = Min(_bytesPerRow, data.LongLength - pointer);
                 int curGrouping = 0;
                 for (long i = 0L; i != chunkSize; i++)
                 {
                     centerSB.AppendFormat("{0:X2}", data[pointer + i]);
                     curGrouping++;
-                    if (curGrouping == grouping && i != chunkSize - 1)
+                    if (curGrouping == _grouping && i != chunkSize - 1)
                     {
                         curGrouping = 0;
                         centerSB.Append(" ");
@@ -60,31 +60,31 @@ public class HexDisplay
                 for (long i = 0L; i != chunkSize; i++)
                 {
                     
-                    if (dispType == DisplayType.Byte)
+                    if (_dispType == DisplayType.Byte)
                     {
                         previewSB.AppendFormat("{0} ", (int)data[pointer + i]);
                     }
-                    else if (dispType == DisplayType.Byte2)
+                    else if (_dispType == DisplayType.Byte2)
                     {
                         previewSB.AppendFormat("{0} ", BitConverter.ToInt16(new byte[] { data[pointer + i], data[pointer + i + 1 ] }, 0));
                         i += 1;
                     }
-                    else if (dispType == DisplayType.Byte4)
+                    else if (_dispType == DisplayType.Byte4)
                     {
                         previewSB.AppendFormat("{0} ", BitConverter.ToInt32(new byte[] { data[pointer + i], data[pointer + i + 1], data[pointer + i + 2], data[pointer + i + 3] }, 0));
                         i += 3;
                     }
-                    else if (dispType == DisplayType.Byte8)
+                    else if (_dispType == DisplayType.Byte8)
                     {
                         previewSB.AppendFormat("{0} ", BitConverter.ToInt32(new byte[] { data[pointer + i], data[pointer + i + 1], data[pointer + i + 2], data[pointer + i + 3], data[pointer + i + 4], data[pointer + i + 5], data[pointer + i + 6], data[pointer + i + 7] }, 0));
                         i += 7;
                     }
-                    else if (dispType == DisplayType.Float)
+                    else if (_dispType == DisplayType.Float)
                     {
                         previewSB.AppendFormat("{0} ", BitConverter.ToSingle(new byte[] { data[pointer + i], data[pointer + i + 1], data[pointer + i + 2], data[pointer + i + 3] }, 0));
                         i += 3;
                     }
-                    else if (dispType == DisplayType.Double)
+                    else if (_dispType == DisplayType.Double)
                     {
                         previewSB.AppendFormat("{0} ", BitConverter.ToDouble(new byte[] { data[pointer + i], data[pointer + i + 1], data[pointer + i + 2], data[pointer + i + 3], data[pointer + i + 4], data[pointer + i + 5], data[pointer + i + 6], data[pointer + i + 7] }, 0));
                         i += 7;
@@ -104,15 +104,57 @@ public class HexDisplay
                 pointer += chunkSize;
             }
 
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Offset (h)", GUILayout.Width(90.0f));
+            GUILayout.Label("Data", GUILayout.Width(_dataWidth));
+            GUILayout.Label("Preview");
+            GUILayout.EndHorizontal();
 
             //Vector2 temp = EditorGUILayout.BeginScrollView(scroll, GUI.skin.horizontalScrollbar, GUIStyle.none);
+            GUILayout.BeginHorizontal();
+
+            GUILayout.BeginVertical();
             GUILayout.BeginHorizontal();
             GUILayout.TextArea(indexSB.ToString(), _style, GUILayout.Width(90.0f));
             GUILayout.TextArea(centerSB.ToString(), _style, GUILayout.Width(_dataWidth));
             GUILayout.TextArea(previewSB.ToString(), _style);
-            scroll.y = GUILayout.VerticalScrollbar(scroll.y, currentRow, 0.0f, data.LongLength / (long)bytesPerRow, GUILayout.ExpandHeight(true));
-            scroll.y = scroll.y >= 0 ? Mathf.Round(scroll.y / bytesPerRow) * bytesPerRow : 0.0f;
             GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            _goToAddressText = GUILayout.TextField(_goToAddressText, _style, GUILayout.Width(90.0f));
+            if (GUILayout.Button("Go", GUILayout.Width(50.0f)))
+            {
+                long address = Convert.ToInt32(_goToAddressText, 16);
+                if (address < 0L)
+                {
+                    address = 0L;
+                }
+                else if (address >= data.LongLength - (16L * (_MAXROWS - 1)))
+                {
+                    address = data.LongLength - (16L * (_MAXROWS - 1));
+                }
+                else
+                {
+                    address = (long)Mathf.Round((int)address / _bytesPerRow) * _bytesPerRow;
+                }
+                scroll.y = address;
+            }
+
+            GUILayout.Space(30.0f);
+            _bytesPerRow = EditorGUILayout.IntPopup(_bytesPerRow, new string[] { "8 bytes per row", "16 bytes per row", "24 bytes per row", "32 bytes per row" }, new int[] { 8, 16, 24, 32 }, GUILayout.Width(120.0f));
+            _grouping = EditorGUILayout.IntPopup(_grouping, new string[] { "1 byte per group", "2 bytes per group", "4 bytes per group", "8 bytes per group" }, new int[] { 1, 2, 4, 8 }, GUILayout.Width(120.0f));
+            GUILayout.Space(60.0f);
+            _dispType = (HexDisplay.DisplayType)EditorGUILayout.EnumPopup("Preview format", _dispType, GUILayout.Width(240.0f));
+
+            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
+
+            scroll.y = GUILayout.VerticalScrollbar(scroll.y, currentRow, 0.0f, Min(data.LongLength, data.LongLength - (16L * (_MAXROWS-2))), GUILayout.ExpandHeight(true));
+            scroll.y = scroll.y >= 0 ? Mathf.Round(scroll.y / _bytesPerRow) * _bytesPerRow : 0.0f;
+
+            GUILayout.EndHorizontal();
+
+            
 
             //EditorGUILayout.EndScrollView();
             return scroll;
